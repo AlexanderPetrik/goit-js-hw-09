13ad2e8f04f981f403a078b085a3d2c2dcf09975
AlexanderPetrik <painkiller408@gmail.com>
2023-05-29 21:50:28 +0000

    Deploying to gh-pages from @ AlexanderPetrik/goit-js-hw-09@65184086e52cb92ed8731c882d7f7acadb1b2e49 🚀

--- 01-color-switcher.377e0548.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector('button[adta-action = "start"]'),document.querySelector('button[adta-action = "stop"]'),document.body;startBtn.addEventListener("click",(function(){})),stopBtn.addEventListener("click",(function(){}))}();
-//# sourceMappingURL=01-color-switcher.377e0548.js.map

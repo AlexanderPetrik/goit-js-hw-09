fcee03c3603e967f6e1e0f8bd77c1c60d03ed5f2
AlexanderPetrik <painkiller408@gmail.com>
2023-05-29 21:25:18 +0300

    01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  btnStart: document.querySelector('button[adta-action = "start"]'),
+  btnStop: document.querySelector('button[adta-action = "stop"]'),
+  body: document.body,
+}
 
+const delay = 1000;
+const interval = null;
+
+// генерация цвета
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+// добавляет рандомный цвет на body
+function getBodyColor() {
+    return body.style.backgroundColor = getRandomHexColor();
+};
+
+
+// обработка событий
+startBtn.addEventListener('click', startBtnHandler);
+stopBtn.addEventListener('click', stopBtnHandler);
+
+function startBtnHandler() { };
+function stopBtnHandler() { };
